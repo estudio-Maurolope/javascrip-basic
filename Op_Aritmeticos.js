b44cb66a09d8fdd2ea298d655cf90bb32f426452
Mauricio López <mauriciolopezosorio99@gmail.com>
2024-04-20 14:48:37 -0500

    Ejercicios con operedores aritméticos

---
+++ Op_Aritmeticos.js
@@ -0,0 +1,22 @@
+const resultadoSuma = 10 + 5;
+console.log(`El resultado de la suma es ${resultadoSuma}`);
+
+
+const resultadoResta = 20 - 8;
+console.log(`El resultado de la resta es ${resultadoResta}`);
+
+
+const resultadoMultiplicacion = 3* 4;
+console.log(`El resultado de la multiplicacion es ${resultadoMultiplicacion}`);
+
+
+const resultadoDivision = 16/4;
+console.log(`El resultado de la division es ${resultadoDivision}`)
+
+
+
+const resultadoResiduo = 10 % 3;
+console.log(`El resultado del residuo es ${resultadoResiduo}`)
+
+
+
