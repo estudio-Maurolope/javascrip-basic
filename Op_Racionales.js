2b3cb0541745e23aaeeec8dcaceb50ce1ae0129d
Mauricio López <mauriciolopezosorio99@gmail.com>
2024-04-24 15:48:26 -0500

    Ejercicios con operadores racionales

---
+++ Op_Racionales.js
@@ -0,0 +1,51 @@
+//Igualdad (==): Evalúa si dos operandos son iguales en valor y tipo.
+const resultadoIgualdad = 10 == "10"; // Output: true (ambos son 1
+console.log(resultadoIgualdad);
+
+//Desigualdad (!=): Evalúa si dos operandos son diferentes en valor o tipo.
+const resultadoDesigualdad = 15 != "15"; // Output: true (diferent
+console.log(resultadoDesigualdad);
+
+//Menor que (<): Compara si un operando es menor que otro en valor numérico.
+const resultadoMenorQue = 8 < 12; // Output: true
+console.log(resultadoMenorQue);
+
+//Mayor que (>): Compara si un operando es mayor que otro en valor numérico.
+const resultadoMayorQue = 20 > 10; // Output: true
+console.log(resultadoMayorQue);
+
+//Menor o igual que (<=): Compara si un operando es menor o igual que otro en valor numérico.
+const resultadoMenorOIgualQue = 15 <= 15; // Output: true
+console.log(resultadoMenorOIgualQue);
+
+//Mayor o igual que (>=): Compara si un operando es mayor o igual que otro en valor numérico.
+const resultadoMayorOIgualQue = 22 >= 20; // Output: true
+console.log(resultadoMayorOIgualQue);
+
+//Validar la edad de un usuario
+const edadUsuario = 18;
+const edadMinima = 18;
+if (edadUsuario >= edadMinima) {
+ console.log("El usuario es mayor o igual a la edad mínima");
+} else {
+ console.log("El usuario no cumple con la edad mínima");
+}
+
+//Verificar si un número es par o impar
+const numero = 15;
+if (numero % 2 === 0) {
+ console.log("El número es par");
+} else {
+ console.log("El número es impar");
+}
+
+//Determinar si un estudiante ha aprobado un curso
+const notaExamen = 65;
+const notaMinimaAprobacion = 70;
+if (notaExamen >= notaMinimaAprobacion) {
+ console.log("El estudiante ha aprobado el curso");
+} else {
+ console.log("El estudiante no ha aprobado el curso");
+ }
+
+
